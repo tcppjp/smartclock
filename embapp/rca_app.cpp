cae21c3cb47d94e59fa53395b23c429da4d1e786
yvt <tcpp.jp@gmail.com>
2016-09-14 16:02:59 +0900

    クリーンアップ

--- embapp/rca_app.cpp
+++ embapp/rca_app.cpp
@@ -25,11 +25,5 @@ void setup()
 
 void loop()
 {
-	SeeedOled.setTextXY(0, 0);
-	SeeedOled.putString("sN0WwH1T3");
-	SeeedOled.setTextXY(1, 0);
-	char msgbuf[32];
-	sprintf(msgbuf, "%02d:%02d:%02d", hour(), minute(), second());
-	SeeedOled.putString(msgbuf);
-	dly_tsk(1000);
+	slp_tsk();
 }
